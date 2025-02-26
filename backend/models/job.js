16def022b9051170f078781d07855028e93b7a3a
Tysum <vinamraraaz@gmail.com>
2025-02-27 00:30:22 +0530

    Implemented Job Schema

---
+++ backend/models/job.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+const jobSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  description: { type: String, required: true },
+  requirements: [{ type: String }],
+  salary: { type: Number },
+  location: { type: String },
+  jobtype: { type: String, required: true },
+  NumberOfPositions: { type: Number, required: true },
+  company: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Company",
+    required: true,
+  },
+  createdBy: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  applications: [{ type: mongoose.Schema.Types.ObjectId, ref: "Application" }],
+});
+export const Job = mongoose.model("Job", jobSchema);
