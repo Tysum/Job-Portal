94a5d582244b911b4b9c007da2be20399fd7d9a1
Tysum <vinamraraaz@gmail.com>
2025-03-10 18:39:26 +0530

    Added functionality to fetch applied jobs, get applicants and to change application status to Application Controller class

--- backend/controller/applicationController.js
+++ backend/controller/applicationController.js
@@ -49,3 +49,93 @@ export const aaplyJob = async (req, res, next) => {
     next(error);
   }
 };
+
+export const getAppliedJobs = async (req, res, next) => {
+  try {
+    const { userId } = req.user;
+    const applications = await Application.find({ applicant: userId })
+      .sort({ createdAt: -1 })
+      .populate({
+        path: "job",
+        options: { sort: { createdAt: -1 } },
+        populate: {
+          path: "company",
+          options: { sort: { createdAt: -1 } },
+        },
+      });
+
+    if (!applications) {
+      return res.status(404).json({
+        message: "No applications found.",
+        success: false,
+      });
+    }
+
+    return res.status(200).json({
+      applications,
+      success: true,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getApplicants = async (req, res, next) => {
+  try {
+    const { jobId } = req.params.id;
+    const job = await Job.findById(jobId).populate({
+      path: "applications",
+      options: { sort: { createdAt: -1 } },
+      populate: {
+        path: "applicant",
+      },
+    });
+
+    if (!job) {
+      return res.status(404).json({
+        message: "Job not found.",
+        success: false,
+      });
+    }
+
+    return res.status(200).json({
+      job,
+      success: true,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const updateStatus = async (req, res, next) => {
+  try {
+    const { applicationId } = req.params.id;
+    const { status } = req.body;
+
+    if (!status) {
+      return res.status(400).json({
+        message: "Status not found.",
+        success: false,
+      });
+    }
+
+    const application = await Application.findById(applicationId);
+    if (!application) {
+      return res.status(404).json({
+        message: "Application not found.",
+        success: false,
+      });
+    }
+
+    application.status = status.toLowerCase();
+    await application.save();
+
+    return res.status(200).json({
+      message: "Status updated successfully.",
+      success: true,
+      application,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
